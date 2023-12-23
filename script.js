5f884612223aee03544ade81281bf04be8d2409a
Syeda Shiza Rizvi <shiza07rizvi@gmail.com>
2023-12-23 20:21:29 +0500

    Added dynamic calendar

--- script.js
+++ script.js
@@ -15,3 +15,23 @@ $(document).ready(function() {
             // Add other Slick options as needed
         });
     });
+
+function handleCalendarButtonClick() {
+    // Open a new tab and load calender.html
+  var newTab = window.open('dyn_calender/calendar.html', '_blank');
+  if (newTab) {
+    newTab.focus();
+  } else {
+    alert("Popup blocker may be preventing the new tab from opening.");
+  }
+}
+    
+    // Function to toggle the visibility of the dropdown content
+function toggleDropdown() {
+    var dropdown = document.getElementById("calendarDropdown");
+    dropdown.style.display = (dropdown.style.display === "block") ? "none" : "block";
+}
+    
+// Attach the toggleDropdown function to the click event of the dropdown button
+document.getElementById("calendarDropdownButton").addEventListener("click", toggleDropdown);
+    
